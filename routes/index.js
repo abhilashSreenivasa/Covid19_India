6dab832cb1f3e90ad3fe4a3c68d46b149045568b
abhilashSreenivasa <abhilash.sreenivasa@gmail.com>
2020-05-21 02:34:06 +0530

    fetching total api

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const fetch=require('node-fetch')
+router.get('/',async (req, res) => {
 
-router.get('/', (req, res) => {
-  res.render('index')
+    try{
+        let response = await fetch(`https://api.covid19india.org/data.json`)
+        let data = await response.json()
+        data.cases_time_series[0]
+   const todayTotal=data.cases_time_series.find((d)=>d.date=="20 May ")
+
+  res.render('index',{todayTotal:todayTotal})
+    }
+    catch(e){
+        console.error(e)
+    }
 })
+
+
 
 module.exports = router
